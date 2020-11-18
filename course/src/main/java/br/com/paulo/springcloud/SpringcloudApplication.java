8d56de5c1e66d691c3d02d3c7cf312874c7b4b2f
paulo.vescovi <paulo.vescovi@ciass.com.br>
2020-11-17 21:50:34 -0300

    iniciando eureka

--- course/src/main/java/br/com/paulo/springcloud/SpringcloudApplication.java
+++ course/src/main/java/br/com/paulo/springcloud/SpringcloudApplication.java
@@ -2,8 +2,12 @@ package br.com.paulo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan({"br.com.paulo.springcloud.model"})
+@EnableJpaRepositories({"br.com.paulo.springcloud.repository"})
 public class SpringcloudApplication {
 
     public static void main(String[] args) {
